10db818bbdc141a01f828f901451e8991c8b1b96
Kaya Tacer <ktacer@proton.me>
2025-02-08 00:46:43 -0500

    integrate button to backend

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,13 +1,20 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron/main');
+const { execFile } = require('node:child_process');
+const path = require('node:path');
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        show: false,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        }
     });
 
+    win.maximize();
+    win.show();
+
     win.loadFile('index.html');
-}
+};
 
 app.whenReady().then(() => {
     createWindow();
@@ -16,3 +23,21 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
     app.quit();
 });
+
+ipcMain.handle('generate', async () => {
+    const board = generateSudokuBoard();
+    return board;
+});
+
+function generateSudokuBoard() {
+    return new Promise((resolve, reject) => {
+        const executablePath = path.join(__dirname, 'dist', 'test');
+        execFile(executablePath, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(JSON.parse(stdout));
+        });
+    });
+};
---
+++ my-electron-app/renderer.js
@@ -0,0 +1,14 @@
+const generateButton = document.getElementById('generate');
+const subgrids = document.getElementsByClassName('subgrid');
+
+generateButton.addEventListener('click', async () => {
+    const board = await window.sudoku.generate();
+
+    for (let i = 0; i < subgrids.length; i++) {
+        const cells = subgrids[i].getElementsByClassName('cell');
+        for (let j = 0; j < cells.length; j++) {
+            cells[j].innerText = board[i][j];
+        }
+    }
+    console.log(board);
+});
